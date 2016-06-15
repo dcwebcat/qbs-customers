e5380b20e07dce741e30efd2ccb10fafc37cec7a
datapike <datapike@gmail.com>
2016-06-15 16:19:19 +0000

    test mailer production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,12 +82,12 @@ Rails.application.configure do
 config.action_mailer.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'copyquality.com',
+  :domain => 'qbs-customers.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
   }
-  config.action_mailer.default_url_options = { :host => 'qbs-customers.herokuapp.com/', :protocol => 'https'}
+  config.action_mailer.default_url_options = { :host => 'qbs-customers.herokuapp.com', :protocol => 'https'}
   
 end

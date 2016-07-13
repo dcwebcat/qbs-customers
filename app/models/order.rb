282767b083bdca0b60cec70323e4aa88f3f1edd3
datapike <datapike@gmail.com>
2016-07-13 15:50:32 +0000

    add postage meters to support

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order
     include ActiveModel::Model
-    attr_accessor :name, :email, :phone, :equipment_id, :black_toner, :cyan_toner, :magenta_toner, :yellow_toner, :waste_toner, :postage_ink, :postage_labels, :postage_sealant, :staples, :current_user
+    attr_accessor :name, :email, :phone, :equipment_id, :black_toner, :cyan_toner, :magenta_toner, :yellow_toner, :waste_toner, :postage_ink, :postage_labels, :postage_sealant, :staples, :wide_format, :current_user
     validates :name, :email, :phone, :equipment_id, presence: true
 end

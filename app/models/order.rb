61a06003834ece08591aa9e938f88636c47c16ce
datapike <datapike@gmail.com>
2016-08-18 14:53:36 +0000

    fix orders service id

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order
     include ActiveModel::Model
-    attr_accessor :name, :email, :phone, :equipment_id, :black_toner, :cyan_toner, :magenta_toner, :yellow_toner, :waste_toner, :postage_ink, :postage_labels, :postage_sealant, :staples, :wide_format, :supply_message, :current_user
-    validates :name, :email, :phone, :equipment_id, presence: true
+    attr_accessor :name, :email, :phone, :service_id, :black_toner, :cyan_toner, :magenta_toner, :yellow_toner, :waste_toner, :postage_ink, :postage_labels, :postage_sealant, :staples, :wide_format, :supply_message, :current_user
+    validates :name, :email, :phone, :service_id, presence: true
 end
